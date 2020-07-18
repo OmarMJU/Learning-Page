560b96ef58c68e929fbefc1bf86f9928c27ea653
Omar M Juarez <omarmju906p@hotmail.com>
2020-07-18 18:03:44 -0500

    Se agrega el Footer.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/components/Footer.scss";
+
+const Footer = () => (
+    <footer className="footer">
+        <a href="/">T&eacute;rminos de uso</a>
+        <a href="/">Declaraci&oacute;n de privacidad</a>
+        <a href="/">Centro de ayuda</a>
+    </footer>
+);
+
+export default Footer;
