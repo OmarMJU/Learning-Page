08410a620723713cb131b4d6b216bbbeb655eb81
Omar M Juarez <omarmju906p@hotmail.com>
2020-08-12 21:03:47 -0500

    Se agrega la página para error 404.

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../assets/styles/components/NotFound.scss";
+
+const NotFound = () => (
+    <section className="mainNot">
+        <div className="main__contenedor">
+            <h1><i className="fas fa-exclamation-triangle"></i></h1>
+            <h1>404</h1>
+            <h2>P&aacute;gina no encontrada</h2>
+            <h3>Un grupo de simios avanzados esta trabajando en el problema.</h3>
+            <h4>Env&iacute;e el siguiente texto para reportar el problema: DSDEFSDFF#ASDASDDA#DASDASDAFTHJKJKL-3GDFDFSD#HJPGM</h4>
+            <h4>No les env&iacute;e captura o imagen porque se asustan.</h4>
+        </div>
+    </section>
+);
+
+export default NotFound;
