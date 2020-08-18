5b2588d7ba879cde98899b33485aef49fc4423c3
Omar M Juarez <omarmju906p@hotmail.com>
2020-08-17 21:43:52 -0500

    Se agrega acción de agregar a favoritos.

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { setFavorite } from "../acctions";
 import "../assets/styles/components/CarouselItems.scss";
 
-const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
-    <div className="carrusel-item">
-        <img className="carrusel-item__imagen" src={cover} alt={title} />
-        <div className="carrusel-item__detalles">
-            <div>
-                <i className="fas fa-play"></i>
-                <i className="fas fa-plus-circle"></i>
+const CarouselItem = (props) => {
+    const { cover, title, year, contentRating, duration } = props;
+
+    const handleSetFavorites = () => {
+        props.setFavorite({
+            cover, title, year, contentRating, duration
+        });
+    }
+
+    return (
+        <div className="carrusel-item">
+            <img className="carrusel-item__imagen" src={cover} alt={title} />
+            <div className="carrusel-item__detalles">
+                <div>
+                    <i className="fas fa-play"></i>
+                    <i className="fas fa-plus-circle" onClick={handleSetFavorites}></i>
+                </div>
+                <p className="carrusel-item__detalles--titulo">{title}</p>
+                <p className="carrusel-item__detalles--subtitulo">{`${year} ${contentRating} ${duration}`}</p>
             </div>
-            <p className="carrusel-item__detalles--titulo">{title}</p>
-            <p className="carrusel-item__detalles--subtitulo">{`${year} ${contentRating} ${duration}`}</p>
         </div>
-    </div>
-);
+    );
+};
+
+const mapDispatchToProps = {
+    setFavorite
+}
 
 CarouselItem.propTypes = {
     cover: PropTypes.string,
@@ -24,4 +40,4 @@ CarouselItem.propTypes = {
     duration: PropTypes.number
 };
 
-export default CarouselItem;
+export default connect(null, mapDispatchToProps)(CarouselItem);
