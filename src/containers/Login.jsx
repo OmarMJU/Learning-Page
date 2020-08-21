382a89eeab3e7e22d0387c88a83a3458fe0e4875
Omar M Juarez <omarmju906p@hotmail.com>
2020-08-20 20:27:20 -0500

    Se agrega manejo de datos en el formulario del login.

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,38 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../assets/styles/components/Login.scss";
 
-const Login = () => (
-    <section className="login">
-        <section className="login__contenedor">
-            <h2>Inicia Sesi&oacute;n</h2>
-            <form action="" className="login__contenedor--from">
-                <input type="text" className="input" placeholder="Correo"/>
-                <input type="password" className="input" placeholder="Contraseña"/>
-                <button className="boton">Inicia Sesi&oacute;n</button>
-                <div className="login__contenedor-recuerdame">
-                    <label>
-                        <input type="checkbox" name="" id="cbox1" value="checkbox"/> Recu&eacute;rdame
-                    </label>
-                    <a href="/">Olvid&eacute; mi contraseña</a>
-                </div>
-            </form>
-            <section className="login__contenedor--social">
-                <div>
-                    <i className="fab fa-google"></i>
-                    <p>Inicia sesi&oacute;n con Google</p>
-                </div>
-                <div>
-                    <i className="fab fa-twitter"></i>
-                    <p>Inicia sesi&oacute;n con Twitter</p>
-                </div>
+const Login = () => {
+    const [form, setValues] = useState({
+        email: ""
+    });
+
+    const handleInput = event => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(form);
+    }
+
+    return (
+        <section className="login">
+            <section className="login__contenedor">
+                <h2>Inicia Sesi&oacute;n</h2>
+                <form action="" className="login__contenedor--from" onSubmit={handleSubmit}>
+                    <input name="email" type="text" className="input" placeholder="Correo" onChange={handleInput}/>
+                    <input name="password" type="password" className="input" placeholder="Contraseña" onChange={handleInput}/>
+                    <button className="boton">Inicia Sesi&oacute;n</button>
+                    <div className="login__contenedor-recuerdame">
+                        <label>
+                            <input type="checkbox" name="" id="cbox1" value="checkbox"/> Recu&eacute;rdame
+                        </label>
+                        <a href="/">Olvid&eacute; mi contraseña</a>
+                    </div>
+                </form>
+                <section className="login__contenedor--social">
+                    <div>
+                        <i className="fab fa-google"></i>
+                        <p>Inicia sesi&oacute;n con Google</p>
+                    </div>
+                    <div>
+                        <i className="fab fa-twitter"></i>
+                        <p>Inicia sesi&oacute;n con Twitter</p>
+                    </div>
+                </section>
+                <p className="login__contenedor--registro">
+                    &iquest;No tienes cuenta?
+                    <Link to="/registrer">  Reg&iacute;strate</Link>
+                </p>
             </section>
-            <p className="login__contenedor--registro">
-                &iquest;No tienes cuenta?
-                <Link to="/registrer">  Reg&iacute;strate</Link>
-            </p>
         </section>
-    </section>
-);
+    )
+}
 
 export default Login;
